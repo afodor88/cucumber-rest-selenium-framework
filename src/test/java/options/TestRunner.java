1574f92544f7a781fc8b29b2d9308c187efb784a
afodor88 <afodor88@gmail.com>
2022-11-28 20:29:31 +0200

    Adding cucumber reports;

--- src/test/java/options/TestRunner.java
+++ src/test/java/options/TestRunner.java
@@ -5,7 +5,10 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = {"stepDefinitions"}, plugin = { "pretty" }, monochrome = true)
+@CucumberOptions(features = "src/test/resources/features", glue = {"stepDefinitions"}, plugin = {
+        "pretty",
+        "json:target/cucumber-report/cucumber.json",
+        "html:target/cucumber-report/cucumber.html"}, monochrome = true)
 public class TestRunner {
 
 }
