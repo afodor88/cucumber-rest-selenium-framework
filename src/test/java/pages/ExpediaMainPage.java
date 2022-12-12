0f6f4fff53299f520bfa8bcd3ddc7167e5e7bed2
afodor88 <afodor88@gmail.com>
2022-12-12 12:47:11 +0200

    Adding all flights;

--- src/test/java/pages/ExpediaMainPage.java
+++ src/test/java/pages/ExpediaMainPage.java
@@ -21,54 +21,37 @@ public class ExpediaMainPage {
     }
 
 
-    public static WebElement addAnotherFlight(WebDriver driver) {
-        element = driver.findElement(By.xpath("//button[@type='button' and contains(., 'Add another flight')]"));
-        return element;
-    }
-
-//    public static List<WebElement> leavingFromList(WebDriver driver) {
-//        elements = driver.findElements(By.cssSelector("[aria-label=\"Leaving from\"]"));
-//        return elements;
-//    }
-
     public static List<WebElement> leavingFromList(WebDriver driver) {
         elements = driver.findElements(By.xpath("//button[contains(@aria-label,'Leaving from')]"));
         return elements;
     }
 
 
-//    public static List<WebElement> goingToList(WebDriver driver) {
-//        elements = driver.findElements(By.cssSelector("[aria-label=\"Going to\"]"));
-//        return elements;
-//    }
-
     public static List<WebElement> goingToList(WebDriver driver) {
         elements = driver.findElements(By.xpath("//button[contains(@aria-label,'Going to')]"));
         return elements;
     }
 
-    public static WebElement whereAreYouLeavingFrom(WebDriver driver) {
-        element = driver.findElement(By.cssSelector("[placeholder=\"Where are you leaving from?\"]"));
-        return element;
-    }
 
-    public static WebElement whereAreYouGoingTo(WebDriver driver) {
-        element = driver.findElement(By.cssSelector("[placeholder=\"Where are you going to?\"]"));
-        return element;
-    }
-    public static WebElement whereAreYouGoingToEmpty(WebDriver driver) {
-        element = driver.findElement(By.cssSelector("[placeholder=\"Where are you going to?'][value=\"\"]"));
+    public static WebElement whereAreYouLeavingFrom(WebDriver driver, int row) {
+        element = driver.findElement(By.id("location-field-leg" + row + "-origin"));
         return element;
     }
 
 
-    public static WebElement firstResultFound(WebDriver driver) {
-        element = driver.findElement(By.cssSelector(".uitk-typeahead-results strong"));
+    public static WebElement whereAreYouGoingTo(WebDriver driver, int row) {
+        element = driver.findElement(By.id("location-field-leg" + row + "-destination"));
         return element;
     }
+
 
     public static WebElement searchButton(WebDriver driver) {
-        element = driver.findElement(By.cssSelector("[type='submit']"));
+        element = driver.findElement(By.xpath("//*[text()='Search']"));
+        return element;
+    }
+
+    public static WebElement addAnotherFlight(WebDriver driver) {
+        element = driver.findElement(By.xpath("//*[text()='Add another flight']"));
         return element;
     }
 
--- src/test/java/stepDefinitions/ExpediaSteps.java
+++ src/test/java/stepDefinitions/ExpediaSteps.java
@@ -56,131 +56,41 @@ public class ExpediaSteps {
         multiCityButton(driver).click();
 
         Assert.assertTrue(addAnotherFlight(driver).isDisplayed());
-
-
-//        List<Map<String, String>> myFlights = flights.asMaps();
-//        for(Map<String, String> flightMap : myFlights){
-//            for(String flight : flightMap.keySet()){
-//
-//            }
-//        }
+        addAnotherFlight(driver).click();
 
         List<List<String>> allFlights = flights.asLists();
+        Assert.assertEquals(allFlights.size(), 3);
 
-        int flightNr = 0;
+        //Adding all flights
         for (List<String> flight : allFlights) {
             String expectedLeavingFrom = flight.get(0);
             String expectedGoingTo = flight.get(1);
-            System.out.println("Flight number: " + (flightNr + 1));
+            int flightRow = allFlights.indexOf(flight);
 
+            leavingFromList(driver).get(flightRow).click();
+            whereAreYouLeavingFrom(driver, flightRow + 1).sendKeys(expectedLeavingFrom);
+            whereAreYouLeavingFrom(driver, flightRow + 1).sendKeys(Keys.ENTER);
+            System.out.println("Leaving from: " + expectedLeavingFrom);
 
-            //LEAVING FROM
-            //clicking Leaving from, adding flight then pressing Enter
-            if (flightNr == 1) {
-                String currentLeavingFrom = leavingFromList(driver).get(flightNr).getText();
-                String[] splitedLeavingFrom = currentLeavingFrom.split("\n");
-                currentLeavingFrom = splitedLeavingFrom[1];
-                Assert.assertEquals(expectedLeavingFrom, currentLeavingFrom);
-            } else {
-                leavingFromList(driver).get(flightNr).click();
-                whereAreYouLeavingFrom(driver).sendKeys(expectedLeavingFrom);
-                whereAreYouLeavingFrom(driver).sendKeys(Keys.ENTER);
-                System.out.println("Leaving from: " + expectedLeavingFrom);
+            //removing 'Leaving from' text
+            String currentLeavingFrom = leavingFromList(driver).get(flightRow).getText();
+            String[] splitedLeavingFrom = currentLeavingFrom.split("\n");
+            currentLeavingFrom = splitedLeavingFrom[1];
+            Assert.assertEquals(expectedLeavingFrom, currentLeavingFrom);
 
-                //removing 'Leaving from' text
-                String currentLeavingFrom = leavingFromList(driver).get(flightNr).getText();
-                String[] splitedLeavingFrom = currentLeavingFrom.split("\n");
-                currentLeavingFrom = splitedLeavingFrom[1];
-
-                Assert.assertEquals(expectedLeavingFrom, currentLeavingFrom);
-            }
-
-
-            //GOING TO
-            //clicking Going to, adding flight then pressing Enter
-            //TODO FIX for flight 2
-            goingToList(driver).get(flightNr).click();
-            whereAreYouGoingTo(driver).sendKeys(expectedGoingTo);
-            whereAreYouGoingTo(driver).sendKeys(Keys.ENTER);
+            goingToList(driver).get(flightRow).click();
+            whereAreYouGoingTo(driver, flightRow + 1).sendKeys(expectedGoingTo);
+            whereAreYouGoingTo(driver, flightRow + 1).sendKeys(Keys.ENTER);
             System.out.println("Going to: " + expectedGoingTo);
 
 
             //removing 'Going to' text
-            String currentGoingTo = goingToList(driver).get(flightNr).getText();
+            String currentGoingTo = goingToList(driver).get(flightRow).getText();
             String[] splittedGoingTo = currentGoingTo.split("\n");
             currentGoingTo = splittedGoingTo[1];
             System.out.println(currentGoingTo);
             Assert.assertEquals(expectedGoingTo, currentGoingTo);
-
-            flightNr++;
         }
-
-
-//        for(int i = 0; i < numberOfFlights;i++){
-//            System.out.println("Flight number: " + i + 1);
-//            leavingFromList(driver).get(i).click();
-//
-//            String leavingFromExpected = flights.get(i);
-//            System.out.println("Leaving from: " + leavingFromExpected);
-//            whereAreYouLeavingFrom(driver).sendKeys(leavingFromExpected);
-//            whereAreYouLeavingFrom(driver).sendKeys(Keys.ENTER);
-//
-//            //removing 'Leaving from' text
-//            String leavingFromFound = leavingFromList(driver).get(i).getText();
-//            String[] splitedLeavingFrom = leavingFromFound.split("\n");
-//            leavingFromFound = splitedLeavingFrom[1];
-//
-//            Assert.assertEquals(leavingFromExpected, leavingFromFound);
-//
-//        }
-
-//        List<List<String>> dataList = dataTable.asLists(String.class);
-
-//        System.out.println("Number of flights: " + numberOfFlights);
-
-//        for(int i = 0; i < numberOfFlights; i++){
-//            //Flight 1: clicking Leaving from
-//            leavingFromList(driver).get(i).click();
-//            String leavingFromF1 = dataList.get(i).get(i);
-//            System.out.println("Searching for: " + leavingFromF1);
-//            whereAreYouLeavingFrom(driver).sendKeys(leavingFromF1);
-//            whereAreYouLeavingFrom(driver).sendKeys(Keys.ENTER);
-//
-//            //removing 'Leaving from' text F1
-//            String leavingFromfoundTextF1 = leavingFromList(driver).get(i).getText();
-//            String[] splitedLeavingFrom = leavingFromfoundTextF1.split("\n");
-//            leavingFromfoundTextF1 = splitedLeavingFrom[1];
-//
-//            System.out.println(leavingFromfoundTextF1);
-//
-//            Assert.assertEquals(leavingFromF1, leavingFromfoundTextF1);
-//
-//            //Flight 1: clicking Going to
-//            goingToList(driver).get(i).click();
-//            String goingToF1 = dataList.get(i).get(i + 1);
-//            System.out.println("Searching for: " + goingToF1);
-//            whereAreYouGoingTo(driver).sendKeys(goingToF1);
-//            whereAreYouGoingTo(driver).sendKeys(Keys.ENTER);
-//
-//            //removing 'Going to' text from F1
-//            String goingToFoundTextF1 = goingToList(driver).get(i).getText();
-//            String[] splitedGoingToF1 = goingToFoundTextF1.split("\n");
-//            goingToFoundTextF1 = splitedGoingToF1[1];
-//
-//            System.out.println(goingToFoundTextF1);
-//
-//            Assert.assertEquals(goingToF1, goingToFoundTextF1);
-//
-//
-//
-//            //removing 'Leaving from' text from F2
-//            String leavingFromfoundTextF2 = goingToList(driver).get(i).getText();
-//            String[] splitedGoingToF2 = leavingFromfoundTextF2.split("\n");
-//            leavingFromfoundTextF2 = splitedGoingToF2[1];
-//            Assert.assertEquals(goingToF1, leavingFromfoundTextF2);
-
-
-//        }
 
 
     }
